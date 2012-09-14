0ae469bc5cb200744419f722b9084fc74735469e
marc <marc@urbanairship.com>
2012-09-14 14:35:41 -0700

    remove selective process logic from application

--- RichPushSample/src/com/urbanairship/richpush/sample/RichPushApplication.java
+++ RichPushSample/src/com/urbanairship/richpush/sample/RichPushApplication.java
@@ -1,14 +1,11 @@
 package com.urbanairship.richpush.sample;
 
-import android.app.ActivityManager;
 import android.app.Application;
-import android.content.Context;
+
 import com.urbanairship.UAirship;
 import com.urbanairship.push.PushManager;
 import com.urbanairship.richpush.RichPushManager;
 import com.urbanairship.richpush.RichPushMessageJavaScript;
-
-import java.util.List;
 
 public class RichPushApplication extends Application {
 
@@ -22,23 +19,7 @@ public class RichPushApplication extends Application {
     @Override
     public void onCreate() {
         UAirship.takeOff(this);
-        if (this.isProcess(this.getPackageName())) {
-            PushManager.enablePush();
-            PushManager.shared().setIntentReceiver(PushReceiver.class);
-            RichPushManager.setJavascriptInterface(RichPushMessageJavaScript.class, "urbanairship");
-        }
+        PushManager.shared().setIntentReceiver(PushReceiver.class);
+        RichPushManager.setJavascriptInterface(RichPushMessageJavaScript.class, "urbanairship");
     }
-
-    private boolean isProcess(String processName) {
-        Context context = getApplicationContext();
-        ActivityManager actMgr = (ActivityManager)context.getSystemService(Context.ACTIVITY_SERVICE);
-        List<ActivityManager.RunningAppProcessInfo> appList = actMgr.getRunningAppProcesses();
-        for (ActivityManager.RunningAppProcessInfo info : appList) {
-            if (info.pid == android.os.Process.myPid() && processName.equals(info.processName)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
 }
