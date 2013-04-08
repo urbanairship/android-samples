7ec90b248c426e96d30ba6cf29e5dec6eb67379f
Ryan Lepinski <ryan.lepinski@urbanairship.com>
2013-04-08 13:28:55 -0700

    Revert "Add TaskBuilder to create proper navigation while pressing the back key"
    This reverts commit b5306678b6b5c314e07ea879af8eedacf7848499.

--- RichPushSample/src/com/urbanairship/richpush/sample/PushReceiver.java
+++ RichPushSample/src/com/urbanairship/richpush/sample/PushReceiver.java
@@ -4,10 +4,10 @@
 
 package com.urbanairship.richpush.sample;
 
+import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.support.v4.app.TaskStackBuilder;
 
 import com.urbanairship.Logger;
 import com.urbanairship.push.PushManager;
@@ -19,43 +19,24 @@ public class PushReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (!intent.getAction().equals(PushManager.ACTION_NOTIFICATION_OPENED)) {
-            return;
-        }
+        if (!intent.getAction().equals(PushManager.ACTION_NOTIFICATION_OPENED)) return;
 
-        if (!RichPushManager.isRichPushMessage(intent.getExtras())) {
-            return;
-        }
-
+        if (!RichPushManager.isRichPushMessage(intent.getExtras())) return;
         String messageId = intent.getStringExtra("_uamid");
         Logger.debug("Notified of a notification opened with id " + messageId);
 
         String activityName = intent.getStringExtra(ACTIVITY_NAME_KEY);
-        activityName = "preferences";
-
+        // default to the Inbox
+        Class<? extends Activity> intentClass = InboxActivity.class;
         if ("home".equals(activityName)) {
-            startActivity(context, MainActivity.class, messageId);
+            intentClass = MainActivity.class;
         } else if ("preferences".equals(activityName)) {
-            startActivity(context, PushPreferencesActivity.class, messageId);
-        } else {
-            startActivity(context, InboxActivity.class, messageId);
+            intentClass = PushPreferencesActivity.class;
         }
+        Intent messageIntent = new Intent(context, intentClass);
+        messageIntent.putExtra(RichPushApplication.MESSAGE_ID_RECEIVED_KEY, messageId);
+        messageIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(messageIntent);
     }
 
-
-    private void startActivity(Context context, Class<?> activityClass, String messageId) {
-        Intent intent = new Intent(context, activityClass);
-        intent.putExtra(RichPushApplication.MESSAGE_ID_RECEIVED_KEY, messageId);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-
-        // The stack builder object will contain an artificial back stack for the
-        // started Activity.
-        // This ensures that navigating backward from the Activity leads out of
-        // your application to the Home screen.
-        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
-
-        stackBuilder.addParentStack(activityClass);
-        stackBuilder.addNextIntent(intent);
-        stackBuilder.startActivities();
-    }
 }
