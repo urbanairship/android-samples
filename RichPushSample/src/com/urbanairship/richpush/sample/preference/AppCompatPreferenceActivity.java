050af9a22ed96b43e03cdeed112ac03bc486fb1e
Ryan Lepinski <ryan.lepinski@urbanairship.com>
2015-05-26 17:21:58 -0700

    [MB-1199] Provide an AppCompatPreferencesActivity to support an action bar with the deprecated preferences activity

--- RichPushSample/src/com/urbanairship/richpush/sample/preference/AppCompatPreferenceActivity.java
+++ RichPushSample/src/com/urbanairship/richpush/sample/preference/AppCompatPreferenceActivity.java
@@ -25,5 +25,124 @@ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 package com.urbanairship.richpush.sample.preference;
 
-public class AppCompatPreferenceActivity {
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatDelegate;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * PreferenceActivity that implements the AppCompatDelegate.
+ */
+public class AppCompatPreferenceActivity extends PreferenceActivity {
+
+    private AppCompatDelegate appCompatDelegate;
+
+    @Override
+    public MenuInflater getMenuInflater() {
+        return this.getDelegate().getMenuInflater();
+    }
+
+    @Override
+    public void setContentView(int layoutResID) {
+        this.getDelegate().setContentView(layoutResID);
+    }
+
+    @Override
+    public void setContentView(View view) {
+        this.getDelegate().setContentView(view);
+    }
+
+    @Override
+    public void setContentView(View view, ViewGroup.LayoutParams params) {
+        this.getDelegate().setContentView(view, params);
+    }
+
+    @Override
+    public void addContentView(View view, ViewGroup.LayoutParams params) {
+        this.getDelegate().addContentView(view, params);
+    }
+
+    @Override
+    public void invalidateOptionsMenu() {
+        this.getDelegate().invalidateOptionsMenu();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        this.getDelegate().onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    protected void onTitleChanged(CharSequence title, int color) {
+        super.onTitleChanged(title, color);
+        this.getDelegate().setTitle(title);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        this.getDelegate().installViewFactory();
+        this.getDelegate().onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        this.getDelegate().onPostCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        this.getDelegate().onPostResume();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        this.getDelegate().onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        this.getDelegate().onDestroy();
+    }
+
+    public boolean supportRequestWindowFeature(int featureId) {
+        return this.getDelegate().requestWindowFeature(featureId);
+    }
+
+    public void supportInvalidateOptionsMenu() {
+        this.getDelegate().invalidateOptionsMenu();
+    }
+
+    public ActionBar getSupportActionBar() {
+        return this.getDelegate().getSupportActionBar();
+    }
+
+    public void setSupportActionBar(Toolbar toolbar) {
+        this.getDelegate().setSupportActionBar(toolbar);
+    }
+
+    @Nullable
+    public ActionBarDrawerToggle.Delegate getDrawerToggleDelegate() {
+        return this.getDelegate().getDrawerToggleDelegate();
+    }
+
+    public AppCompatDelegate getDelegate() {
+        if(this.appCompatDelegate == null) {
+            this.appCompatDelegate = AppCompatDelegate.create(this, null);
+        }
+
+        return this.appCompatDelegate;
+    }
 }
